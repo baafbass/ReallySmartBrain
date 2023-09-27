a310777eada156d11f8f2bad7c5335ced6f11e15
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-28 01:26:30 +0300

    the brain get improved

--- brain.py
+++ brain.py
@@ -4,11 +4,10 @@ import os
 exec_path = os.getcwd()
  
 prediction = ImageClassification()
-# SqueezeNet model also no longer exists, now the fastest is MobileNetV2
 prediction.setModelTypeAsMobileNetV2()
 prediction.setModelPath(os.path.join(exec_path, 'mobilenet_v2-b0353104.pth'))
 prediction.loadModel()
 
-predctions, probabilities = prediction.classifyImage(os.path.join(exec_path,'giraffe.jpg'), result_count=5)
+predctions, probabilities = prediction.classifyImage(os.path.join(exec_path,'house.jpg'), result_count=5)
 for eachPred, eachProb in zip(predctions, probabilities):
     print(f'{eachPred} : {eachProb}')
